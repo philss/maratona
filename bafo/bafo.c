59a95b4f5b5a3fe8b1987d441ff70be599c9e780
Philip Sampaio <philip.sampaio@gmail.com>
2011-06-25 03:15:43 -0300

    Bafo - v1

---
+++ bafo/bafo.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+/* Problem code: BAFO */
+
+
+int who_wins(int,int);
+int verify_cases(int);
+int verify_games(int,int);
+
+int main() {
+	char player[2][5] = { "Aldo\0", "Beto\0" };
+	int player_points[3] = { 0, 0, 0 };
+	int winner = 0;
+	int a = 0;
+	int b = 0;
+	int i = 0;
+	int j = 0;
+	int cases = 0;
+	
+	scanf("%d",&cases);
+
+	while ( verify_cases(cases) ) {
+		for(i=0; i < cases; i++) {
+			scanf("%d %d", &a, &b);
+			if ( ! verify_games(a,b) ) continue;
+			winner = who_wins(a,b);
+			player_points[winner]++;
+		}
+		j++;
+		winner = who_wins(player_points[0],player_points[1]);
+		printf("Teste %d\n%s\n\n",j,player[winner]);
+		player_points[0] = 0;
+		player_points[1] = 0;
+		scanf("%d",&cases);
+	}
+	return 0;
+}
+
+int who_wins(int a, int b) {
+	/* if anyone wins, point goes to gaspar */
+	if ( a > b ) return 1;
+	if ( a == b ) return 2;
+	return 0;
+}
+
+int verify_cases(int cases) {
+	if ( cases < 1 ||	cases > 1000 ) return 0;
+	return 1;
+}
+int verify_games(int a, int b) {
+	if ( a < 0 || a > 100 || b < 0 || b > 100 ) return 0;
+	return 1;
+}
