a438dbe4c26ad7208507bbc96525475e7003dbeb
Philip Sampaio <philip.sampaio@gmail.com>
2011-06-30 16:12:58 -0300

    Frotatax v1

---
+++ frotatax/frotatax.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  float a = 0.0, g = 0.0,ra = 0.0,rg = 0.0;
+  while ( scanf("%1.2f %1.2f %1.2f %1.2f",&a,&g,&ra,&rg) ) {
+   printf("TESTE\n"); 
+  }
+
+  return 0;
+}
