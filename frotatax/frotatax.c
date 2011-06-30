a24a87ab3240838c8c03d746eeb5f6954b2c7c94
Philip Sampaio <philip.sampaio@gmail.com>
2011-06-30 17:07:18 -0300

    One line

--- frotatax/frotatax.c
+++ frotatax/frotatax.c
@@ -2,9 +2,8 @@
 
 int main() {
   float a = 0.0, g = 0.0,ra = 0.0,rg = 0.0;
-  while ( scanf("%1.2f %1.2f %1.2f %1.2f",&a,&g,&ra,&rg) ) {
-   printf("TESTE\n"); 
-  }
+  scanf("%1.2f %1.2f %1.2f %1.2f",&a,&g,&ra,&rg);
+  
 
   return 0;
 }
