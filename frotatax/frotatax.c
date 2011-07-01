7d633c4b4142089a967cc27cbb70ed7f1eb8f076
Philip Sampaio <philip.sampaio@gmail.com>
2011-07-01 16:15:37 -0300

    Better tab spacing

--- frotatax/frotatax.c
+++ frotatax/frotatax.c
@@ -2,11 +2,11 @@
 
 int main() {
   float a = 0.0f;
-	float g = 0.0f;
-	float ra = 0.0f;
-	float rg = 0.0f;
+  float g = 0.0f;
+  float ra = 0.0f;
+  float rg = 0.0f;
   scanf("%f %f %f %f",&a, &g, &ra, &rg);
-	printf("%c",( (( rg / g ) >= ( ra / a )) ? 'G' : 'A') );  
+  printf("%c",( (( rg / g ) >= ( ra / a )) ? 'G' : 'A') );  
 
   return 0;
 }
