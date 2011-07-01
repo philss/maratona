392653adb3f2e23a5e0a7ef6003dd54117429413
Philip Sampaio <philip.sampaio@gmail.com>
2011-07-01 02:37:09 -0300

    Frotatax ok - need to get less time

--- frotatax/frotatax.c
+++ frotatax/frotatax.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
 int main() {
-  float a = 0.0, g = 0.0,ra = 0.0,rg = 0.0;
-  scanf("%1.2f %1.2f %1.2f %1.2f",&a,&g,&ra,&rg);
-  
+  float a = 0.0f;
+	float g = 0.0f;
+	float ra = 0.0f;
+	float rg = 0.0f;
+  scanf("%f %f %f %f",&a, &g, &ra, &rg);
+	printf("%c",( (( rg / g ) >= ( ra / a )) ? 'G' : 'A') );  
 
   return 0;
 }
