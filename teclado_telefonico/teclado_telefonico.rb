c2fe097d99ab262f52690bedad6b8462f79c0fc0
Philip Sampaio <philip.sampaio@gmail.com>
2011-07-27 01:21:19 -0300

    Testes falhando ainda... sono, amanha continuo =)

--- teclado_telefonico/teclado_telefonico.rb
+++ teclado_telefonico/teclado_telefonico.rb
@@ -10,8 +10,22 @@ class TecladoTelefonico
                           [],
                           ["A","B","C"],
                           ["D","E","F"],
-                          []
+                          ["G","H","I"],
+                          ["J","K","L"],
+                          ["M","N","O"],
+                          ["P","K","R","S"],
+                          ["T","U","V"],
+                          ["W","X","Y","Z"]
                         ]
     letras_por_teclas[numero]
+  end
+
+  def para_palavras(numero)
+    lista_de_numeros = separar_numeros(numero)
+    #TODO this
+  end
+
+  def separar_numeros(numero)
+    numero.to_s.split("").map{|x| x.to_i }
   end
 end
--- teclado_telefonico/tests_teclado_telefonico.rb
+++ teclado_telefonico/tests_teclado_telefonico.rb
@@ -2,7 +2,13 @@ require 'test/unit'
 require 'teclado_telefonico'
 
 class TestTecladoTelefonico < Test::Unit::TestCase
-  def test_list_of_chars_by_number
+  def test_lista_de_caracteres_por_numero
     assert_equal(["A","B","C"],TecladoTelefonico.new.para_letras(2))
+  end
+  def test_separar_numeros
+    assert_equal([3,2,3,2],TecladoTelefonico.new.separar_numeros(3232))
+  end
+  def test_formar_palavras_por_numeros
+    assert_equal("DADA",TecladoTelefonico.new.para_palavras(3232))
   end
 end
