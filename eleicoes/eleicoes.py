2d2307ae7d5172456ebb49e6f01c30c4c5800f62
Philip Sampaio <philip.sampaio@gmail.com>
2011-10-18 14:40:08 -0200

    Indent

--- eleicoes/eleicoes.py
+++ eleicoes/eleicoes.py
@@ -6,15 +6,15 @@ i = 0
 winner_points = 0
 size = int(input())
 if size > 0 and size <= 100000:
-		while i < size:
-				vote = int(input())
-				if vote > 0 and vote <= 1000000000:
-						if not count.has_key(vote):
-								count[vote] = 1
-						else:
-								count[vote] += 1
-						if count[vote] > winner_points:
-								winner = vote
-								winner_points = count[vote]
-				i += 1
+  while i < size:
+    vote = int(input())
+    if vote > 0 and vote <= 1000000000:
+      if not count.has_key(vote):
+        count[vote] = 1
+      else:
+        count[vote] += 1
+      if count[vote] > winner_points:
+        winner = vote
+        winner_points = count[vote]
+        i += 1
 print winner
