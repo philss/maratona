ca351cef642b95dd185cd3283cfd8b9fb62efd05
Philip Sampaio <philip.sampaio@gmail.com>
2011-12-06 08:32:52 -0200

    Quermesse

---
+++ querm/querm.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+  int i = 0,j = 0, n = 0, p = 0;
+  while (scanf("%d",&n) != EOF && n) {
+    printf("Teste %d\n",i+1);
+    for(j=0; j < n; j++) {
+      scanf("%d",&p);
+      if ( p == j+1) {
+        printf("%d\n",p);
+        break;
+      }
+      p = 0;
+    }
+    i++;
+  }
+  return 0;
+}
