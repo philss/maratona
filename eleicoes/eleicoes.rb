f0c11fdb625729f0b78cf857d87126075cdb8855
Philip Sampaio <philip.sampaio@gmail.com>
2012-01-14 03:34:03 -0200

    Tentativa com Symbols em ruby

--- eleicoes/eleicoes.rb
+++ eleicoes/eleicoes.rb
@@ -7,14 +7,14 @@ if size > 0 and size <= 100000
   size.times do
     vote = gets.to_i
     if vote > 0 and vote <= 1000000000
-      if count[vote] == nil
-        count[vote] = 1
+      if count[vote.to_s.to_sym]
+        count[vote.to_s.to_sym] += 1
       else
-        count[vote] += 1
+        count[vote.to_s.to_sym] = 1
       end
-      if count[vote] > winner_points
+      if count[vote.to_s.to_sym] > winner_points
         winner = vote
-        winner_points = count[vote]
+        winner_points = count[vote.to_s.to_sym]
       end
     end
   end
